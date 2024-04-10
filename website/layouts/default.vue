34a736428dca4a7bb324c65451b21cdb4193e03e
Mario Arturo Sanchez Ruelas <marioarturosanchez@gmail.com>
2024-04-10 10:52:50 -0600

    Starting to add the initial UI design

---
+++ website/layouts/default.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-layout>
+        <v-navigation-drawer>
+            <v-list lines="two">
+                <NuxtLink to="/" style="text-decoration: none;">
+                    <v-list-item prepend-icon="mdi-view-dashboard" title="Panel"> </v-list-item>
+                </NuxtLink>
+                <NuxtLink to="/" style="text-decoration: none;">
+                    <v-list-item prepend-icon="mdi-account-circle" title="Cursos"> </v-list-item>
+                </NuxtLink>
+                <NuxtLink to="/" style="text-decoration: none;">
+                    <v-list-item prepend-icon="mdi-account-circle" title="Perfil"> </v-list-item>
+                </NuxtLink>
+                <v-divider color="orange"></v-divider>
+                <v-spacer></v-spacer>
+                <v-btn to="/login">
+                    <p>Iniciar Sesión</p>
+                </v-btn>
+                <v-btn to="/register">
+                    <p>Registrase</p>
+                </v-btn>
+            </v-list>
+        </v-navigation-drawer>
+        <v-main>
+            <slot />
+        </v-main>
+    </v-layout>
+</template>

eb159dfd07a2f3dee48de959c47674744aee3f45
Mario Arturo Sanchez Ruelas <marioarturosanchez@gmail.com>
2024-04-13 08:07:37 -0600

    Mejora Página Perfil

--- website/pages/perfil.vue
+++ website/pages/perfil.vue
@@ -1,3 +1,15 @@
 <template>
-    
+    <div class="d-flex justify-center fill-hight">
+        <v-sheet :elevation="3" width="500" height="600" class="d-flex justify-center mt-15">
+            <div>
+                <v-avatar size="200" color="#79827B" class="mt-8"></v-avatar>
+                <v-text-field variant="solo" class="mt-10" label="Nombre"></v-text-field>
+                <v-text-field variant="solo" label="Correo Electrónico"></v-text-field>
+                <v-text-field variant="solo" label="Contraseña"></v-text-field>
+                <v-btn color="#EE3A1D" width="200" prepend-icon="mdi-logout">
+                    <p>Cerrar Sesión</p>
+                </v-btn>
+            </div>
+        </v-sheet>
+    </div>
 </template>

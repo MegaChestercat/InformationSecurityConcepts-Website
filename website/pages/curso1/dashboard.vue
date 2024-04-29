a27c8e00c0e80e0c84859105bf3dbfc811f4cf84
Engel02 <angel.morenolo@udlap.mx>
2024-04-29 15:27:25 -0600

    agregar videos a contenido adicional

--- website/pages/curso1/dashboard.vue
+++ website/pages/curso1/dashboard.vue
@@ -4,7 +4,7 @@
             <v-img height="270" width="100vw" src="~~/assets/auth_bg.png" cover>
                <div class="d-flex justify-center align-center fill-height">
                 <div class="d-block">
-                    <p class="text-white text-center text-h3 font-weight-bold">Fundamentos I</p>
+                    <p class="text-white text-center text-h3 font-weight-bold">Introducción a la Seguridad Informática</p>
                     <p class="text-white text-center text-subtitle-2 font-weight-bold mt-5">Este módulo presenta información con respecto a temas introductorios de seguridad informática para sentar las bases para temas subsecuentes.</p>
                 </div>
                </div>
@@ -96,12 +96,88 @@
                 </div>
             </v-sheet>
 
-            <v-sheet :elevation="5" width="75vw" height="150" class="ml-15 my-10 rounded-xl" >
-                <div class="d-flex align-center fill-height">
-                    <p class="ml-8 text-black text-h5 font-weight-bold">Contenido Adicional</p>
-                    <v-icon class="option ml-4" size="x-large">mdi-chevron-down</v-icon>
-                </div>
-            </v-sheet>
+            <v-menu>
+        <template v-slot:activator="{ props: menu }">
+          <v-sheet :elevation="5" width="75vw" height="150" class="ml-15 my-10 rounded-xl" v-bind="menu">
+            <div class="d-flex align-center fill-height">
+              <p class="ml-8 text-black text-h5 font-weight-bold">Contenido Adicional</p>
+              <v-icon class="option ml-4" size="x-large">mdi-chevron-down</v-icon>
+            </div>
+          </v-sheet>
+        </template>
+        <v-sheet>
+          <!-- Aquí puedes agregar las opciones del menú para Contenido Adicional -->
+          <!-- Por ejemplo: -->
+          <v-sheet :elevation="5" width="75vw" height="350" class="mx-15 mt-8 rounded-xl">
+  <NuxtLink to="" style="text-decoration: none;">
+    <div class="d-flex align-center fill-height">
+      <!-- Vista previa del video de YouTube -->
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/LupJ9Z9xlC8?si=iBkry3z8gFSAv_gE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+      <!-- Descripción de la opción -->
+      <div class="ml-8">
+        <p class="text-black text-h5 font-weight-bold">Tema: Amenazas de Ciberseguridad</p>
+        
+      </div>
+    </div>
+  </NuxtLink>
+</v-sheet>
+
+          <v-sheet :elevation="5" width="75vw" height="350" class="mx-15 mt-8 rounded-xl">
+            <NuxtLink to="/curso2/contenido-adicional/opcion1" style="text-decoration: none;">
+              <div class="d-flex align-center fill-height">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/mzXNU5vLNDc?si=l0yGqCgKlPJjgUvu" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                <p class="ml-8 text-black text-h5 font-weight-bold">Tema: Web: Superficie, Deep y Dark</p>
+                
+              </div>
+             
+            </NuxtLink>
+          </v-sheet>
+          <v-sheet :elevation="5" width="75vw" height="350" class="mx-15 mt-8 rounded-xl">
+            <NuxtLink to="/curso2/contenido-adicional/opcion1" style="text-decoration: none;">
+              <div class="d-flex align-center fill-height">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/ZpCV3oa-1E8?si=UrPol42mgnYwNZ8R" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                <p class="ml-8 text-black text-h5 font-weight-bold">Tema: Tendencias en la Seguridad</p>
+                
+              </div>
+             
+            </NuxtLink>
+          </v-sheet>
+          <v-sheet :elevation="5" width="75vw" height="350" class="mx-15 mt-8 rounded-xl">
+            <NuxtLink to="/curso2/contenido-adicional/opcion1" style="text-decoration: none;">
+              <div class="d-flex align-center fill-height">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/O8SeYEFnBmQ?si=HFE5NyC__3FTMyIv" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                <p class="ml-8 text-black text-h5 font-weight-bold">Tema: Modelo OSI</p>
+                
+              </div>
+             
+            </NuxtLink>
+          </v-sheet>
+          <v-sheet :elevation="5" width="75vw" height="350" class="mx-15 mt-8 rounded-xl">
+            <NuxtLink to="/curso2/contenido-adicional/opcion1" style="text-decoration: none;">
+              <div class="d-flex align-center fill-height">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/EHjmxujXIaQ?si=ac7q_nrbCP6zKxaY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                <p class="ml-8 text-black text-h5 font-weight-bold">Tema: Mecanismos de Seguridad</p>
+                
+              </div>
+             
+            </NuxtLink>
+          </v-sheet>
+          <v-sheet :elevation="5" width="75vw" height="350" class="mx-15 mt-8 rounded-xl">
+            <NuxtLink to="/curso2/contenido-adicional/opcion1" style="text-decoration: none;">
+              <div class="d-flex align-center fill-height">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/s1qnXTfeXMI?si=zeynpxhwqpV4j9rW" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                <p class="ml-8 text-black text-h5 font-weight-bold">Tema: Biométricos</p>
+                
+              </div>
+             
+            </NuxtLink>
+          </v-sheet>
+          <!-- Repite este patrón para cada opción del menú de Contenido Adicional -->
+        </v-sheet>
+      </v-menu>
+              
+            
        </div>
     </div>
 </template>
@@ -114,4 +190,9 @@
 .option:hover{
     color: #1DF02D;
 }
+
+iframe{
+    margin-left: 3%;
+}
+
 </style>
--- website/pages/curso3/dashboard.vue
+++ website/pages/curso3/dashboard.vue
@@ -4,7 +4,7 @@
             <v-img height="270" width="100vw" src="~~/assets/auth_bg.png" cover>
                <div class="d-flex justify-center align-center fill-height">
                 <div class="d-block">
-                    <p class="text-white text-center text-h3 font-weight-bold">Fundamentos I</p>
+                    <p class="text-white text-center text-h3 font-weight-bold">Cifrado Simétrico</p>
                     <p class="text-white text-center text-subtitle-2 font-weight-bold mt-5">Este módulo presenta información con respecto a temas introductorios de seguridad informática para sentar las bases para temas subsecuentes.</p>
                 </div>
                </div>
@@ -96,12 +96,70 @@
                 </div>
             </v-sheet>
 
-            <v-sheet :elevation="5" width="75vw" height="150" class="ml-15 my-10 rounded-xl" >
-                <div class="d-flex align-center fill-height">
-                    <p class="ml-8 text-black text-h5 font-weight-bold">Contenido Adicional</p>
-                    <v-icon class="option ml-4" size="x-large">mdi-chevron-down</v-icon>
-                </div>
-            </v-sheet>
+            <v-menu>
+        <template v-slot:activator="{ props: menu }">
+          <v-sheet :elevation="5" width="75vw" height="150" class="ml-15 my-10 rounded-xl" v-bind="menu">
+            <div class="d-flex align-center fill-height">
+              <p class="ml-8 text-black text-h5 font-weight-bold">Contenido Adicional</p>
+              <v-icon class="option ml-4" size="x-large">mdi-chevron-down</v-icon>
+            </div>
+          </v-sheet>
+        </template>
+        <v-sheet>
+          <!-- Aquí puedes agregar las opciones del menú para Contenido Adicional -->
+          <!-- Por ejemplo: -->
+          <v-sheet :elevation="5" width="75vw" height="350" class="mx-15 mt-8 rounded-xl">
+  <NuxtLink to="" style="text-decoration: none;">
+    <div class="d-flex align-center fill-height">
+      <!-- Vista previa del video de YouTube -->
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/uzGzSHGip_o?si=J9KCz5p89BDTldPm" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+      <!-- Descripción de la opción -->
+      <div class="ml-8">
+        <p class="text-black text-h5 font-weight-bold">Tema: Symmetric Cipher</p>
+        
+      </div>
+    </div>
+  </NuxtLink>
+</v-sheet>
+
+          <v-sheet :elevation="5" width="75vw" height="350" class="mx-15 mt-8 rounded-xl">
+            <NuxtLink to="/curso2/contenido-adicional/opcion1" style="text-decoration: none;">
+              <div class="d-flex align-center fill-height">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/WjATtFxR2F0?si=a2f1vof8lzgp7KRy" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                <p class="ml-8 text-black text-h5 font-weight-bold">Tema: Técnicas de Substitución</p>
+                
+              </div>
+             
+            </NuxtLink>
+          </v-sheet>
+          <v-sheet :elevation="5" width="75vw" height="350" class="mx-15 mt-8 rounded-xl">
+            <NuxtLink to="/curso2/contenido-adicional/opcion1" style="text-decoration: none;">
+              <div class="d-flex align-center fill-height">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/3gnJtKNbM-A?si=pbQ0ZWdY_i6oe3TK" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                <p class="ml-8 text-black text-h5 font-weight-bold">Tema: Esteganografía</p>
+                
+              </div>
+             
+            </NuxtLink>
+          </v-sheet>
+          <v-sheet :elevation="5" width="75vw" height="350" class="mx-15 mt-8 rounded-xl">
+            <NuxtLink to="/curso2/contenido-adicional/opcion1" style="text-decoration: none;">
+              <div class="d-flex align-center fill-height">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/LPWRerMSQHs?si=-wR59IHrSlPEtOEQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                <p class="ml-8 text-black text-h5 font-weight-bold">Tema: DES</p>
+                
+              </div>
+             
+            </NuxtLink>
+          </v-sheet>
+          
+         
+          <!-- Repite este patrón para cada opción del menú de Contenido Adicional -->
+        </v-sheet>
+      </v-menu>
+              
+            
        </div>
     </div>
 </template>
@@ -113,5 +171,9 @@
 
 .option:hover{
     color: #1DF02D;
+}
+
+iframe{
+    margin-left: 3%;
 }
 </style>
